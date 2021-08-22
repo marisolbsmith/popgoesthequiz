81db6f94bd84e4b2a47c51ca72f5a237ad279acd
mrssmith <marisolbsmith@yahoo.com>
2021-08-21 23:22:58 -0500

    working

--- Assets/script.js
+++ Assets/script.js
@@ -1,47 +1,136 @@
-var question1 = "What is Jquery";
-var question1A = "is a fast, small, and feature-rich JavaScript library.";
-var question1B =
-  "scripting or programming language that allows you to implement complex features on web pages";
-var question1C = "All of the above";
-var question1D = "None of above";
+var questions_answers = [
+  [
+    "What is Jquery",
+    "is a fast, small, and feature-rich JavaScript library.",
+    "scripting or programming language that allows you to implement complex features on web pages",
+    "All of the above",
+    "None of above",
+    "answer1",
+  ],
+  [
+    "Which of the following is correct?",
+    "jQuery is a JSON libary",
+    "jQuery is a Javascript libary",
+    "jQuery is a web framwork",
+    "None of the above.",
+    "answer2",
+  ],
+  [
+    "What attribute in the <script> element must point to a copy of jQuery?",
+    "src",
+    "href",
+    "onclick",
+    "script",
+    "answer1",
+  ],
+  ["What character is an alias in jQuery", "?", "()", "!", "$", "answer4"],
+  [
+    "What does following code do $( document ).ready(function()",
+    "The page can be manipulated  until the document is ready.",
+    "The page can't be manipulated  until the document is ready.",
+    "All of the above",
+    "None of the above",
+    "answer2",
+  ],
+];
 
-var question2 = "Which of the following is correct?";
-var question2A = "jQuery is a JSON libary";
-var question2B = "jQuery is a Javascript libary";
-var question2C = "jQuery is a web framwork";
-var question2D = "None of the above.";
+var correct = 0;
+var incorrect = 0;
+var clock = 6000;
+var gameOverFlag = false;
 
-var question3 =
-  "What attribute in the <script> element must point to a copy of jQuery?";
-var question3A = "src";
-var question3B = "href";
-var question3C = "onclick";
-var question3D = "script";
+setInterval(function () {
+  clock--;
+  document.getElementById("clock").innerHTML = clock;
+  if (clock < 0 && gameOverFlag == false) {
+    gameOverFlag = true;
+    gameOver();
+    //alert("hi")
+  }
+}, 1000);
 
-var question4 = "What character is an alias in jQuery";
-var question4A = "?";
-var question4B = "()";
-var question4C = "!";
-var question4D = "$";
+var gameStarted = false;
+var current_question = 0;
 
-var question5 = "What does following code do $( document ).ready(function()";
-var question5A = "The page can be manipulated  until the document is ready.";
-var question5B = "The page can't be manipulated  until the document is ready.";
-var question5C = "All of the above";
-var question5D = "None of the above";
-
-var current_question = 1;
 function startQuiz() {
-  document.getElementById("question").innerHTML = question1;
-  document.getElementById("answer1").value = question1A;
-  document.getElementById("answer2").value = question1B;
-  document.getElementById("answer3").value = question1C;
-  document.getElementById("answer4").value = question1D;
+  if (gameStarted == false) {
+    clock = 60;
+    gameStarted = true;
+  }
+  enableQuiz();
+  if (current_question <= 4) {
+    document.getElementById("question").innerHTML =
+      questions_answers[current_question][0];
+    document.getElementById("answer1").value =
+      questions_answers[current_question][1];
+    document.getElementById("answer2").value =
+      questions_answers[current_question][2];
+    document.getElementById("answer3").value =
+      questions_answers[current_question][3];
+    document.getElementById("answer4").value =
+      questions_answers[current_question][4];
 
-  document.getElementById("label1").innerHTML = question1A;
-  document.getElementById("label2").innerHTML = question1B;
-  document.getElementById("label3").innerHTML = question1C;
-  document.getElementById("label4").innerHTML = question1D;
+    document.getElementById("label1").innerHTML =
+      questions_answers[current_question][1];
+    document.getElementById("label2").innerHTML =
+      questions_answers[current_question][2];
+    document.getElementById("label3").innerHTML =
+      questions_answers[current_question][3];
+    document.getElementById("label4").innerHTML =
+      questions_answers[current_question][4];
+  } else {
+    gameOver();
+  }
 }
 
-function submitAnswer() {}
+function submitAnswer() {
+  var answer = Array.from(document.getElementsByClassName("answers")).filter(
+    (x) => x["checked"]
+  );
+
+  if (answer[0].id == questions_answers[current_question][5]) {
+    current_question++;
+    correct++;
+    startQuiz();
+  } else {
+    substractFromClock();
+    current_question++;
+    incorrect++;
+    //alert("Wrong")
+    startQuiz();
+  }
+}
+
+function substractFromClock() {
+  clock -= 5;
+}
+
+function gameOver() {
+  gameOverFlag = true;
+  var name = prompt("Please enter your name");
+  alert(
+    "Thanks " +
+      name +
+      ". Your scored " +
+      correct +
+      " correct and " +
+      incorrect +
+      " incorrect."
+  );
+}
+
+function enableQuiz() {
+  document.getElementById("clock").style.display = "block";
+  document.getElementById("thing").style.display = "block";
+
+  document.getElementById("question").style.display = "block";
+  document.getElementById("answer1").style.display = "block";
+  document.getElementById("answer2").style.display = "block";
+  document.getElementById("answer3").style.display = "block";
+  document.getElementById("answer4").style.display = "block";
+
+  document.getElementById("label1").style.display = "block";
+  document.getElementById("label2").style.display = "block";
+  document.getElementById("label3").style.display = "block";
+  document.getElementById("label4").style.display = "block";
+}
